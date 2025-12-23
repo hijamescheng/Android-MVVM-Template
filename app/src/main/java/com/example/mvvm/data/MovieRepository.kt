051d0ee174564fa3f49714ca6ebf7bc622cf2430
james cheng <tojamescheng@gmail.com>
2025-12-23 14:59:36 +1100

    fix code format and update github action flow

--- app/src/main/java/com/example/mvvm/data/MovieRepository.kt
+++ app/src/main/java/com/example/mvvm/data/MovieRepository.kt
@@ -12,55 +12,57 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MovieRepository @Inject constructor(
-    val localDataSource: LocalMovieDataSource,
-    val remoteMovieDataSource: RemoteMovieDataSource,
-    @Named("IODispatcher") val ioDispatcher: CoroutineDispatcher,
-    @Named("DefaultDispatcher") val defaultDispatcher: CoroutineDispatcher,
-    val moshi: Moshi,
-) {
-    private val adapter =
-        moshi.adapter<List<Row>>(
-            Types.newParameterizedType(List::class.java, Row::class.java),
-        )
+class MovieRepository
+    @Inject
+    constructor(
+        val localDataSource: LocalMovieDataSource,
+        val remoteMovieDataSource: RemoteMovieDataSource,
+        @Named("IODispatcher") val ioDispatcher: CoroutineDispatcher,
+        @Named("DefaultDispatcher") val defaultDispatcher: CoroutineDispatcher,
+        val moshi: Moshi,
+    ) {
+        private val adapter =
+            moshi.adapter<List<Row>>(
+                Types.newParameterizedType(List::class.java, Row::class.java),
+            )
 
-    fun observeHomePage(): Flow<Result<List<Row>>> {
-        refreshHomepageIfNeeded()
-        return observeCachedHomePage()
-    }
+        fun observeHomePage(): Flow<Result<List<Row>>> {
+            refreshHomepageIfNeeded()
+            return observeCachedHomePage()
+        }
 
-    // fire and forget -> fetch from remote API and cache result in local storage
-    fun refreshHomepageIfNeeded() {
-        CoroutineScope(ioDispatcher).launch {
-            runCatching {
-                remoteMovieDataSource.getHomePage()
-            }.onSuccess {
-                localDataSource.upsertScreenResponse(
-                    ScreenResponseEntity(
-                        screenName = HOME_SCREEN,
-                        responseJson = adapter.toJson(it),
-                        updatedAt = System.currentTimeMillis(),
-                    ),
-                )
-            }.onFailure {
-                // Fail silently
-                print("")
+        // fire and forget -> fetch from remote API and cache result in local storage
+        fun refreshHomepageIfNeeded() {
+            CoroutineScope(ioDispatcher).launch {
+                runCatching {
+                    remoteMovieDataSource.getHomePage()
+                }.onSuccess {
+                    localDataSource.upsertScreenResponse(
+                        ScreenResponseEntity(
+                            screenName = HOME_SCREEN,
+                            responseJson = adapter.toJson(it),
+                            updatedAt = System.currentTimeMillis(),
+                        ),
+                    )
+                }.onFailure {
+                    // Fail silently
+                    print("")
+                }
             }
         }
-    }
 
-    fun observeCachedHomePage(): Flow<Result<List<Row>>> {
-        // treat local storage as single source of truth
-        // 1. query cached home screen response
-        // 2. start a async request to fetch and cache from remote api
-        return localDataSource.getScreenResponse(HOME_SCREEN).map { queryResult ->
-            queryResult?.let {
-                Result.success(adapter.fromJson(it).orEmpty())
-            } ?: Result.success(emptyList())
-        }.flowOn(defaultDispatcher)
-    }
+        fun observeCachedHomePage(): Flow<Result<List<Row>>> {
+            // treat local storage as single source of truth
+            // 1. query cached home screen response
+            // 2. start a async request to fetch and cache from remote api
+            return localDataSource.getScreenResponse(HOME_SCREEN).map { queryResult ->
+                queryResult?.let {
+                    Result.success(adapter.fromJson(it).orEmpty())
+                } ?: Result.success(emptyList())
+            }.flowOn(defaultDispatcher)
+        }
 
-    companion object {
-        const val HOME_SCREEN = "home"
+        companion object {
+            const val HOME_SCREEN = "home"
+        }
     }
-}
--- app/src/main/java/com/example/mvvm/domain/LoadHomePageUseCase.kt
+++ app/src/main/java/com/example/mvvm/domain/LoadHomePageUseCase.kt
@@ -5,8 +5,10 @@ import com.example.mvvm.data.Row
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class LoadHomePageUseCase @Inject constructor(
-    val repository: MovieRepository,
-) {
-    fun execute(): Flow<Result<List<Row>>> = repository.observeHomePage()
-}
+class LoadHomePageUseCase
+    @Inject
+    constructor(
+        val repository: MovieRepository,
+    ) {
+        fun execute(): Flow<Result<List<Row>>> = repository.observeHomePage()
+    }
